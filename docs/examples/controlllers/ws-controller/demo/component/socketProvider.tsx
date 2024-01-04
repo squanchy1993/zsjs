df6c1d0d2ddd63f7ff3298cd52bf24b25d5d3d5a
squanchy1993 <frundle@hotmail.com>
2024-01-04 19:20:14 +0800

    feat: Change the wsController dir.

--- docs/examples/controlllers/ws-controller/demo/component/socketProvider.tsx
+++ docs/examples/controlllers/ws-controller/demo/component/socketProvider.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { WsContoller } from "@zs-ui/controllers";
+import { WsContoller } from "@zs-ui/ws-controllers";
 
 const WsControllerContext = React.createContext<WsContoller | null>(null);
 
