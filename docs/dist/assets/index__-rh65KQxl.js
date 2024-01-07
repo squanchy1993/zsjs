e23e9df1d18d1d946863c6d2716c9422c11d58ce
squanchy1993 <frundle@hotmail.com>
2024-01-07 20:48:04 +0800

    fix: Add static website.

---
+++ docs/dist/assets/index__-rh65KQxl.js
@@ -0,0 +1,16 @@
+import { j as jsxRuntimeExports } from "./index-D9YKVBLB.js";
+const Page = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { children: [
+    /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Home page" }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "You can customize the whole home page using React." })
+  ] });
+};
+const m0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: Page
+}, Symbol.toStringTag, { value: "Module" }));
+const modules = {};
+modules["main"] = m0;
+export {
+  modules as default
+};
