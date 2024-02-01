853ed7a018b5509bc8130b089249d0bd90b57798
zhusisheng <zhusisheng@shenhaoinfo.com>
2024-02-01 16:33:12 +0800

    feature(resize-fit): Add some new modes.

--- packages/resize-fit/dist/index.mjs
+++ packages/resize-fit/dist/index.mjs
@@ -1,25 +1,35 @@
-class z {
+class v {
   constructor({
-    container: h,
-    target: l,
-    mode: f = "cover"
+    container: l,
+    target: o,
+    mode: f = "fill"
   }) {
     this.resizeObserver = new ResizeObserver((c) => {
-      c.forEach((b) => {
-        let { offsetWidth: t, offsetHeight: s } = this.container, { offsetWidth: i, offsetHeight: r } = this.target, n = s / r, d = t / i;
-        if (this.target.style.transformOrigin = "0 0", this.mode == "cover")
-          this.target.style.transform = `matrix(${d}, 0, 0, ${n}, 0, 0)`;
-        else if (this.mode == "contain") {
-          let e = 0, a = 0, o = 0, m = t / s, g = i / r;
-          m < g ? (e = t / i, o = Math.abs(s - r * e) / 2) : (e = s / r, a = Math.abs(t - i * e) / 2), this.target.style.transform = `matrix(1, 0, 0, 1, ${a}, ${o}) scale(${e})`;
+      c.forEach(($) => {
+        let { offsetWidth: e, offsetHeight: s } = this.container, { offsetWidth: i, offsetHeight: r } = this.target, n = s / r, m = e / i;
+        this.target.style.transformOrigin = "0 0";
+        let t = 0, a = 0, h = 0, g = e / s, b = i / r;
+        switch (this.mode) {
+          case "fill":
+            this.target.style.transform = `matrix(${m}, 0, 0, ${n}, 0, 0)`;
+            break;
+          case "contain":
+            g < b ? (t = e / i, h = Math.abs(s - r * t) / 2) : (t = s / r, a = Math.abs(e - i * t) / 2), this.target.style.transform = `matrix(1, 0, 0, 1, ${a}, ${h}) scale(${t})`;
+            break;
+          case "fitWidth":
+            t = e / i, this.target.style.transform = `matrix(1, 0, 0, 1, ${a}, ${h}) scale(${t})`;
+            break;
+          case "fitHeight":
+            t = s / r, this.target.style.transform = `matrix(1, 0, 0, 1, ${a}, ${h}) scale(${t})`;
+            break;
         }
       });
-    }), this.resizeObserver.observe(h), this.container = h, this.target = l, this.mode = f;
+    }), this.resizeObserver.observe(l), this.container = l, this.target = o, this.mode = f;
   }
   dispose() {
     this.resizeObserver.unobserve(this.container);
   }
 }
 export {
-  z as ResizeFit
+  v as ResizeFit
 };
