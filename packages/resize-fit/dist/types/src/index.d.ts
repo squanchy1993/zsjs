b78bb0b72947ef0934fad98bb8d4c1dd5fc42bb5
zhusisheng <zhusisheng@shenhaoinfo.com>
2024-03-04 15:17:11 +0800

    fix(resizeFit): fix the mode of fitWidth's error and add new mode fitWidthExtendHeight.

--- packages/resize-fit/dist/types/src/index.d.ts
+++ packages/resize-fit/dist/types/src/index.d.ts
@@ -2,11 +2,15 @@ export declare class ResizeFit {
     container: HTMLElement;
     target: HTMLElement;
     mode: string;
+    childElement: {
+        width: number;
+        height: number;
+    };
     resizeObserver: ResizeObserver;
     constructor({ container, target, mode, }: {
         container: HTMLElement;
         target: HTMLElement;
-        mode?: "fill" | "contain" | "fitHeight" | "fitWidth";
+        mode?: "fill" | "contain" | "fitHeight" | "fitWidth" | "fitWidthExtendHeight";
     });
     dispose(): void;
 }
--- packages/resize-fit/src/index.ts
+++ packages/resize-fit/src/index.ts
@@ -2,29 +2,29 @@ export class ResizeFit {
   container: HTMLElement;
   target: HTMLElement;
   mode: string;
+  childElement: { width: number, height: number } = {
+    width: 0,
+    height: 0
+  }
 
   resizeObserver = new ResizeObserver((items) => {
     items.forEach((item) => {
-      let { offsetWidth: fatherWidth, offsetHeight: fatherHeight } =
-        this.container;
-      let { offsetWidth: childWidth, offsetHeight: childHeight } = this.target;
-
-      let skewY = fatherHeight / childHeight;
-      let skewX = fatherWidth / childWidth;
-
-      this.target.style.transformOrigin = "0 0";
+      let { offsetWidth: fatherWidth, offsetHeight: fatherHeight } = this.container;
+      let { width: childWidth, height: childHeight } = this.childElement;
 
       let scale = 0;
       let moveX = 0;
       let moveY = 0;
-      let fatherRate = fatherWidth / fatherHeight;
-      let childRate = childWidth / childHeight;
 
       switch (this.mode) {
         case 'fill':
+          let skewX = fatherWidth / childWidth;
+          let skewY = fatherHeight / childHeight;
           this.target.style.transform = `matrix(${skewX}, 0, 0, ${skewY}, 0, 0)`;
           break;
         case 'contain':
+          let fatherRate = fatherWidth / fatherHeight;
+          let childRate = childWidth / childHeight;
           if (fatherRate < childRate) {
             scale = fatherWidth / childWidth;
             moveY = Math.abs(fatherHeight - childHeight * scale) / 2;
@@ -35,9 +35,15 @@ export class ResizeFit {
           this.target.style.transform = `matrix(1, 0, 0, 1, ${moveX}, ${moveY}) scale(${scale})`;
           break;
         case 'fitWidth':
-          scale = fatherWidth / childWidth;
+          scale = fatherWidth / this.childElement.width;
           // moveY = Math.abs(fatherHeight - childHeight * scale) / 2;
           this.target.style.transform = `matrix(1, 0, 0, 1, ${moveX}, ${moveY}) scale(${scale})`;
+          break;
+        case 'fitWidthExtendHeight':
+          scale = fatherWidth / this.childElement.width;
+          // moveY = Math.abs(fatherHeight - childHeight * scale) / 2;
+          this.target.style.transform = `matrix(1, 0, 0, 1, ${moveX}, ${moveY}) scale(${scale})`;
+          this.target.style.height = `${fatherHeight / scale}px`
           break;
         case 'fitHeight':
           scale = fatherHeight / childHeight;
@@ -57,12 +63,25 @@ export class ResizeFit {
   }: {
     container: HTMLElement;
     target: HTMLElement;
-    mode?: "fill" | "contain" | "fitHeight" | "fitWidth";
+    mode?: "fill" | "contain" | "fitHeight" | "fitWidth" | "fitWidthExtendHeight";
   }) {
-    this.resizeObserver.observe(container);
+    // set container;
     this.container = container;
+    this.container.style.position = 'relative';
+
+    // set target;
     this.target = target;
+    let { offsetWidth: childWidth, offsetHeight: childHeight } = target;
+    this.childElement.width = childWidth;
+    this.childElement.height = childHeight;
+    this.target.style.position = 'absolute';
+    this.target.style.transformOrigin = "0 0";
+
+    // set mode
     this.mode = mode;
+
+    // observe ;
+    this.resizeObserver.observe(container);
   }
 
   dispose() {
