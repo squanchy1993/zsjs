98905b048c45468f6e41079625641753d47ff723
squanchy1993 <squanchy@yeah.net>
2024-04-02 10:11:01 +0800

    feat(utils):add eventCollect.

--- packages/utils/dist/index.mjs
+++ packages/utils/dist/index.mjs
@@ -1,12 +1,12 @@
-function y(r, e = 1e3) {
+function E(r, e = 1e3) {
   let t = !1, s, n = 1e3;
   e && (n = e);
-  const c = async () => {
+  const i = async () => {
     try {
       await r();
     } finally {
       t && (s = setTimeout(() => {
-        clearTimeout(s), c();
+        clearTimeout(s), i();
       }, n));
     }
   };
@@ -14,90 +14,124 @@ function y(r, e = 1e3) {
     start: () => {
       if (t)
         throw new Error("ready start!");
-      t = !0, c();
+      t = !0, i();
     },
     stop: () => t = !1,
-    refresh: c,
+    refresh: i,
     setTime: (f) => {
       n = f;
     }
   };
 }
-function T({
+function x({
   cb: r,
   retryCount: e,
   intervalTime: t,
   event: s
 }) {
-  let n = !1, c = !1, i = () => {
+  let n = !1, i = !1, c = () => {
   };
   return {
     promise: new Promise(async (a, f) => {
-      i = () => {
+      c = () => {
         if (n == !0)
           return;
-        e = 0, c = !0, n = !0;
+        e = 0, i = !0, n = !0;
         const o = "Because of reason [user cancel], re-execute end";
         return console.warn(o), s(o), f(new Error(o));
       };
-      const p = async () => {
+      const m = async () => {
         try {
           const o = await r();
           n = !0, a(o);
         } catch (o) {
-          if (c)
+          if (i)
             return;
-          let u = "";
-          o instanceof Error ? u = o.message : u = JSON.stringify(o);
-          const w = `Because of reason [${u}], start re-execute on ${e}`;
-          if (console.warn(w), s(w), e == 0) {
-            const d = `Because of reason [${u}], re-execute end`;
-            console.error(d), s(d), n = !0, f(o);
+          let l = "";
+          o instanceof Error ? l = o.message : l = JSON.stringify(o);
+          const d = `Because of reason [${l}], start re-execute on ${e}`;
+          if (console.warn(d), s(d), e == 0) {
+            const p = `Because of reason [${l}], re-execute end`;
+            console.error(p), s(p), n = !0, f(o);
           } else
             e !== 0 && (e > 0 && e--, setTimeout(() => {
-              p();
+              m();
             }, t));
         }
       };
-      p();
+      m();
     }),
-    cancel: i
+    cancel: c
   };
 }
-function b(r, e) {
-  let t = (i) => {
+function g(r, e) {
+  let t = (c) => {
   }, s;
   e && (s = setTimeout(() => {
     t("execute function timeout");
   }, e));
-  const n = new Promise((i, m) => {
+  const n = new Promise((c, h) => {
     t = (a = "promise aborted") => {
-      m(new g(a));
+      h(new w(a));
     };
   });
   return {
-    promise: Promise.race([r, n]).then((i) => (s && (clearTimeout(s), s = void 0), i)),
+    promise: Promise.race([r, n]).then((c) => (s && (clearTimeout(s), s = void 0), c)),
     abort: t
   };
 }
-class g extends Error {
+class w extends Error {
   constructor(e) {
     super(e);
   }
 }
-function l(r) {
-  return r && (Array.isArray(r) ? r.map((e) => l(e)) : r instanceof Date ? new Date(r) : typeof r == "object" ? Object.fromEntries(
-    Object.entries(r).map(([e, t]) => [e, l(t)])
+function u(r) {
+  return r && (Array.isArray(r) ? r.map((e) => u(e)) : r instanceof Date ? new Date(r) : typeof r == "object" ? Object.fromEntries(
+    Object.entries(r).map(([e, t]) => [e, u(t)])
   ) : r);
 }
-function x(r) {
-  const e = l(r);
-  return { data: r, defaultData: e, getClone: () => l(e) };
+function y(r) {
+  const e = u(r);
+  return { data: r, defaultData: e, getClone: () => u(e) };
+}
+class T {
+  constructor(e) {
+    this.events = {}, e.forEach((t) => {
+      Reflect.set(this.events, t, []);
+    });
+  }
+  /**
+   * Add a callback function
+   * @param {string} eventName - The event name.
+   * @param {function} fun - The callback function.
+   */
+  addEventListener(e, t) {
+    if (!Reflect.has(this.events, e))
+      throw new Error(`event ${e} doesn't exist!`);
+    this.events[e].includes(t) || this.events[e].push(t);
+  }
+  /**
+  * remove a callback function
+  * @param {string} eventName - The event name.
+  * @param {function} fun - The callback function.
+  */
+  removeEventListener(e, t) {
+    if (!Reflect.has(this.events, e))
+      throw new Error(`event ${e} doesn't exist!`);
+    const s = this.events[e].indexOf(t);
+    s !== -1 && this.events[e].splice(s, 1);
+  }
+  dispatchEvent(e, t) {
+    if (!Reflect.has(this.events, e))
+      throw new Error(`event ${e} doesn't exist!`);
+    this.events[e].forEach((s) => s(t));
+  }
 }
 export {
-  b as cancelablePromise,
-  x as dataWithDefault,
-  l as deepClone,
-  y as loopFunc,
-  T as reExecute
+  T as EventsCollect,
+  g as cancelablePromise,
+  y as dataWithDefault,
+  u as deepClone,
+  E as loopFunc,
+  x as reExecute
 };
--- packages/utils/dist/types/src/index.d.ts
+++ packages/utils/dist/types/src/index.d.ts
@@ -3,4 +3,5 @@ export * from './reExecute';
 export * from './cancelablePromise';
 export * from './logic';
 export * from './deepClone';
+export * from './eventsCollect';
 //# sourceMappingURL=index.d.ts.map
