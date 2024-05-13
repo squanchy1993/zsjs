3ac45c49f022455b6bdecd50ccec57573fe7f5ea
squanchy1993 <frundle@hotmail.com>
2024-05-13 11:00:57 +0800

    fix: add  async

---
+++ packages/utils/src/asyncPromise.ts
@@ -0,0 +1,27 @@
+export function asyncPromise<P, R>(fun: (parameters: P) => Promise<R>) {
+  let loginCB: { succeed: Function[], fail: Function[] } = {
+    succeed: [],
+    fail: []
+  }
+  let isLocked = false
+  return async function (parameters: P): Promise<R> {
+    return new Promise(async (resolve, reject) => {
+      loginCB.succeed.push(resolve)
+      loginCB.fail.push(reject)
+
+      if (!isLocked) {
+        isLocked = true
+        try {
+          const data = await fun(parameters)
+          loginCB.succeed.map(fun => fun(data))
+        } catch (error) {
+          loginCB.fail.map(fun => fun(error))
+        } finally {
+          loginCB.succeed = []
+          loginCB.fail = []
+          isLocked = false
+        }
+      }
+    })
+  }
+}
